617f24e86a516a7937dd50bda93b570fd1edb3b7
aridan <adnanbukvic862@gmail.com>
2024-05-30 19:07:28 +0200

    added animation

--- src/scripts/general.js
+++ src/scripts/general.js
@@ -4,6 +4,7 @@ window.onload = function () {
     twemoji.parse(document.body,
         { folder: 'svg', ext: '.svg' } // This is to specify to Twemoji to use SVGs and not PNGs
     );
+    document.querySelector('#feed').classList.add('swipe-up');
 }
 
 function showMenuSettings(){

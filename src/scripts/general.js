999fbb3a0ccf0ed77c62c2018413c5c2a846a240
aridan <adnanbukvic862@gmail.com>
2024-06-01 23:25:20 +0200

    added experimental details page

--- src/scripts/general.js
+++ src/scripts/general.js
@@ -17,6 +17,31 @@ function slideDown() {
     element.classList.add('slideDown');
     // Wait for the duration of the animation (0.25 seconds) before redirecting
     setTimeout(function() {
-        window.history.back();
-    }, 100); // 250 milliseconds = 0.25 seconds
+        // Check if the previous URL contains 'chirp.aridan.net' or '127.0.0.1'
+        var referrer = document.referrer;
+        if (referrer.indexOf('chirp.aridan.net') !== -1 || referrer.indexOf('127.0.0.1') !== -1) {
+            // If yes, go back in history
+            window.history.back();
+        } else {
+            // If not, redirect to chirp.aridan.net
+            window.location.href = 'https://chirp.aridan.net';
+        }
+    }, 250); // 250 milliseconds = 0.25 seconds
+}
+
+function back() {
+    var element = document.getElementById('feed');
+    element.classList.add('slideDown');
+    // Wait for the duration of the animation (0.25 seconds) before redirecting
+    setTimeout(function() {
+        // Check if the current URL contains 'chirp.aridan.net' or '127.0.0.1'
+        var currentURL = window.location.href;
+        if (currentURL.indexOf('chirp.aridan.net') === -1 && currentURL.indexOf('127.0.0.1') === -1) {
+            // If not, redirect to chirp.aridan.net
+            window.location.href = 'https://chirp.aridan.net';
+        } else {
+            // If yes, go back in history
+            window.history.back();
+        }
+    }, 250); // 250 milliseconds = 0.25 seconds
 }

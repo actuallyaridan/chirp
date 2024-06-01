a7e7423eb7cbf7c35e4c05c2c3ae011c521d764c
aridan <adnanbukvic862@gmail.com>
2024-06-01 23:45:32 +0200

    fix redirects

--- src/scripts/general.js
+++ src/scripts/general.js
@@ -7,7 +7,7 @@ window.onload = function () {
     document.querySelector('#feedCompose').classList.add('swipe-up');
 }
 
-function showMenuSettings(){
+function showMenuSettings() {
     document.getElementById("menuSettings").classList.toggle("visible");
     document.getElementById("settingsButtonWrapper").classList.toggle("clickedDown");
 }
@@ -15,33 +15,21 @@ function showMenuSettings(){
 function slideDown() {
     var element = document.getElementById('feedCompose');
     element.classList.add('slideDown');
-    // Wait for the duration of the animation (0.25 seconds) before redirecting
-    setTimeout(function() {
-        // Check if the previous URL contains 'chirp.aridan.net' or '127.0.0.1'
+    setTimeout(function () {
         var referrer = document.referrer;
-        if (referrer.indexOf('chirp.aridan.net') !== -1 || referrer.indexOf('127.0.0.1') !== -1) {
-            // If yes, go back in history
+        if (referrer.indexOf('chirp.aridan.net') !== -1) {
             window.history.back();
         } else {
-            // If not, redirect to chirp.aridan.net
             window.location.href = 'https://chirp.aridan.net';
         }
-    }, 250); // 250 milliseconds = 0.25 seconds
+    }, 250);
 }
 
 function back() {
-    var element = document.getElementById('feed');
-    element.classList.add('slideDown');
-    // Wait for the duration of the animation (0.25 seconds) before redirecting
-    setTimeout(function() {
-        // Check if the current URL contains 'chirp.aridan.net' or '127.0.0.1'
-        var currentURL = window.location.href;
-        if (currentURL.indexOf('chirp.aridan.net') === -1 && currentURL.indexOf('127.0.0.1') === -1) {
-            // If not, redirect to chirp.aridan.net
-            window.location.href = 'https://chirp.aridan.net';
-        } else {
-            // If yes, go back in history
-            window.history.back();
-        }
-    }, 250); // 250 milliseconds = 0.25 seconds
+    var referrer = document.referrer || "";
+    if (referrer.indexOf('chirp.aridan.net') !== -1) {
+        window.history.back();
+    } else {
+        window.location.href = 'https://chirp.aridan.net';
+    }
 }

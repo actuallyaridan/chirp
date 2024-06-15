5f10d63f46ec5a2fe3a706272fb2973f47dd5f60
Dexrn ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2024-06-14 21:08:36 -0700

    potentially janky code.

--- compose/submit.php
+++ compose/submit.php
@@ -1,5 +1,28 @@
 <?php
 try {
+    /* ===== Dexrn =====
+     * This stuff should be turned into an API instead, so you can communicate from the frontend using JS...
+     * Especially the header redirect at the bottom... shouldn't the client do that upon OK????
+     * Additionally, you could set up API keys, which in my opinion should be free to all, with SOME measures in place to prevent spam and other malicious acts.
+     * also I am working on little sleep so some of this code might be shoddy.
+    */
+
+    /* ===== Dexrn =====
+     * This could very well be not the greatest place to put this (especially with how this codebase is)... but I am gonna put it here, hopefully temporarily.
+     * also, there may need to be good sanitization done with this, not too sure though.
+    */
+    $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : "none";
+    $allowedHosts = ['']; // add hosts to the variable as you see fit.
+    /* ===== Dexrn =====
+     * ^ This should probably be moved to somewhere with a more global scope, if possible.
+     * same with the $host var.
+    */
+    
+    if ($host === "none" || !in_array($host, $allowedHosts)) {
+        header($_SERVER['SERVER_PROTOCOL'].' 400 Bad Request');
+        exit;
+    }
+    
     $db = new PDO('sqlite:../chirp.db');
 
     $sql = "INSERT INTO chirps (chirp, user, timestamp) VALUES (:chirp, :user, :timestamp)";
